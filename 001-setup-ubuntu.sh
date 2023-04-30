7e1bd69ce08781ab05864ee42b01dfc7eb556c64
pwn4all <66910989+pwn4all@users.noreply.github.com>
2023-04-30 20:22:33 +0900

    Update 001-setup-ubuntu.sh

--- 001-setup-ubuntu.sh
+++ 001-setup-ubuntu.sh
@@ -1,25 +1,25 @@
 ####################################################################################
 ## install packages
 ####################################################################################
-sudo apt-get update
-sudo apt-get install build-essential gdb gcc-multilib g++-multilib git tmux
-
+apt-get update
+apt-get install build-essential gcc-multilib g++-multilib -y
+apt-get install gdb gdb-multiarch tmux -y
+apt-get install vim net-tools netcat git curl -y
 
 ####################################################################################
 ## install pwntools
 ####################################################################################
-sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install --upgrade pwntools
-
+apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential -y
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
 
 
 ####################################################################################
 ## install 32bit-libs
 ####################################################################################
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+dpkg --add-architecture i386
+apt-get update
+apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 -y
 
 
 ####################################################################################
@@ -28,6 +28,10 @@ sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
 git clone https://github.com/longld/peda.git /usr/local/peda
 echo "source /usr/local/peda/peda/peda.py" >> ~/.gdbinit
 
+git clone https://github.com/pwndbg/pwndbg /usr/local/pwndbg
+cd /usr/local/pwndbg && ./setup.sh
+echo 'source /usr/local/pwndbg/gdbinit.py' >> ~/.gdbinit
+
 curl -L https://raw.githubusercontent.com/hugsy/gef/dev/gef.py -o /usr/local/gef/gef.py
 echo "source /usr/local/gef/gef.py" >> ~/.gdbinit
 
@@ -35,13 +39,13 @@ echo "source /usr/local/gef/gef.py" >> ~/.gdbinit
 ####################################################################################
 ## install ROPgadget
 ####################################################################################
-sudo apt-get install python3-capstone
-sudo python3 -m pip install ropgadget
+apt-get install python3-capstone -y
+python3 -m pip install ropgadget
 
 
 ####################################################################################
 ## install one_gadget
 ####################################################################################
-sudo apt-get install ruby
-sudo gem install one_gadget
+apt-get install ruby -y
+gem install one_gadget
 
